32c4224ef20f8704c12775807dc9756387740ada
Ania <anna.stempel@gmail.com>
2023-04-17 21:42:59 +0100

    how to declare and initialise string examples

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -2,10 +2,33 @@ package main
 
 import "fmt"
 
-func Hello() string {
-return "Hello, world"
+func Hello(name string) string {
+/* 	s1:= "test2"
+	h:= fmt.Sprintf("Hello, %s %s", name, s1)
+	h:= fmt.Sprintf("Hello, %s", name)
+ */
+/* 	var s2 string	// declare string
+	s2 = ", "		// initialize variable, note only = was used
+	// s2:=", "		// decalre and initialize, :=
+	h:= "Hello" + s2 + name // decalre and initialize (string concat) */
+
+	/* const s3 = "Hello, "
+	h:= s3 + name */
+
+	var s4 string = "Hello, " // homework var s4 = "Hello, "
+	h:= s4 + name
+	return h
+
+// got %q want %q", got, want
+// return "Hello, " + name
+
 }
 
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("apple"))
 }
+
+// Shift+Alt+/
+/* func main() {
+	fmt.Println(Hello("apple"))
+} */
