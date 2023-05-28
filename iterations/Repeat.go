b035e5b38b8ed4b2106e375d50cc9fb407ea550d
Ania <anna.stempel@gmail.com>
2023-05-28 23:42:34 +0100

    Iterations - benchmarks and test examples

--- iterations/Repeat.go
+++ iterations/Repeat.go
@@ -1,21 +1,51 @@
+
 package iteration
+import (
+	"fmt"
+	"errors"
+	"log"
+)
+
 
 const numberOfRepeats = 5
 
 // accept a character and return it repeated numberOfRepeates times
 func RepeatN(c string) string {
 	var repeatedC string
-	for i := 0; i < numberOfRepeats; i++ {
-		repeatedC += c
+	validated, err := CharValidation(c)
+	if err != nil {
+		// %v
+		//repeatedC = fmt.Sprintf("%s",err) 
+		log.Fatal(fmt.Sprintf("%s",err) )
+	} else {
+		for i := 0; i < numberOfRepeats; i++ {
+			repeatedC += validated
+		}
 	}
 	return repeatedC
 }
 
+// accept a character and validates if it's a single character
+func CharValidation(c string) (validated string, err error) {
+
+	length := len(c)
+
+	if length == 0 {
+		err = errors.New("No character was provided") 
+	}
+	if length > 1 {
+		err = errors.New("This is not a character")
+	}
+
+	return c, err
+}
+
 // accept a character and return it repeated 5 times
-func Repeat(c string) string {
+func Repeat(c string) string {	
 	var repeated string // decalre string variable
 	for i := 0; i < 5; i++ {
 		repeated = repeated + c
 	}
 	return repeated
+	
 }
--- iterations/repeat_test.go
+++ iterations/repeat_test.go
@@ -1,6 +1,22 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"log"
+	"testing"
+)
+
+/*
+helper function accepts a "test", the acctual and expected value
+and fails the test with an error message when the expected value
+doesn't match the acctual vaue
+*/
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
 
 func TestRepeat(t *testing.T) {
 	repeated := Repeat("a")
@@ -12,10 +28,73 @@ func TestRepeat(t *testing.T) {
 }
 
 func TestRepeatN(t *testing.T) {
-	repeated := Repeat("abc")
-	expected := "abcabcabcabcabc"
+	repeated := RepeatN("z")
+	expected := "zzzzz"
 
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func TestCharValidation(t *testing.T) {
+
+	t.Run("testing when a single character is supplied", func(t *testing.T) {
+		got, err := CharValidation("b")
+		want := "b"
+		if err != nil {
+			log.Fatal(err)
+		}
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("testing when a multiple characters are supplied", func(t *testing.T) {
+		got, err := CharValidation("abc")
+		want := "This is not a character"
+		got = fmt.Sprintf("%s", err)
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("testing when no character is supplied", func(t *testing.T) {
+		validated, err := CharValidation("")
+		var got string
+		want := "No character was provided"
+		if validated == "" {
+			got = fmt.Sprintf("%s", err)
+		}
+		assertCorrectMessage(t, got, want)
+	})
+
+	/*	t.Run("testing when a multiple characters are supplied", func(t *testing.T) {
+			got, err := CharValidation("abc")
+			want := "This is not a character"
+			if err != nil {
+				log.Fatal(err)
+			}
+			assertCorrectMessage(t, got, want)
+		})
+	*/
+}
+
+func ExampleRepeat() {
+	r1 := RepeatN("x")
+	fmt.Println(r1)
+	// Output: xxxxx
+
+}
+
+/*
+	r2 := RepeatN("")
+	fmt.Println(r2)
+	// Output2: "No character was provided"
+
+	r3 := RepeatN("abc")
+	fmt.Println(r3)
+	// Output3: "This is not a character"
+*/
+
+// benchmark code runs b.N times and measures how long it taks
+func BenchmarkRepeatN(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		RepeatN("a")
 	}
 }
