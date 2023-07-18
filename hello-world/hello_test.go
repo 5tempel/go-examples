a06a2c2292b74a04b7a8207132b4cb2926c8d1c8
Ania <anna.stempel@gmail.com>
2023-07-18 22:30:03 +0100

    Added verbose to test; replaced 1 with w in hello_test

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -51,7 +51,7 @@ func TestHelloLanguage(t *testing.T) {
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("in English if unknow language", func(t *testing.T) {
-		got := HelloLanguage("Zosia", "1")
+		got := HelloLanguage("Zosia", "w")
 		want := "Hello, Zosia"
 		assertCorrectMessage(t, got, want)
 	})
@@ -74,7 +74,7 @@ func TestHelloLSwitch(t *testing.T) {
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("in English if unknow language", func(t *testing.T) {
-		got := HelloLSwitch("Zosia", "1")
+		got := HelloLSwitch("Zosia", "w")
 		want := "Hello, Zosia"
 		assertCorrectMessage(t, got, want)
 	})
