01a1445c8155f9d0eb2fab1d2b2b2fe240c8c5cb
Ania <anna.stempel@gmail.com>
2023-08-16 22:12:34 +0100

    Appending and slicing a slice.

--- arrays_slices/sum_test.go
+++ arrays_slices/sum_test.go
@@ -2,8 +2,8 @@ package arrays_slices
 
 import (
 	"fmt"
-	"testing"
 	"reflect"
+	"testing"
 )
 
 /*
@@ -68,21 +68,20 @@ func TestSumSlices(t *testing.T) {
 
 }
 
-
 // TestSumAll - https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/arrays-and-slices#write-the-test-first-2
 func TestSumAll(t *testing.T) {
 
 	got := SumAll([]int{1, 2}, []int{0, 9}, []int{20, 5, 4, 33}, []int{-5, -20, 0, 2})
-	
+
 	// Slice can only be cmpared to nil
 	t.Run("testing if slice is nil", func(t *testing.T) {
-		
+
 		if got == nil {
-		t.Errorf("got = %d is nil", got)
+			t.Errorf("got = %d is nil", got)
 		}
 	})
 
-	// reflect.DeepEqual is useful for seeing if any two variables are the same. 
+	// reflect.DeepEqual is useful for seeing if any two variables are the same.
 	// Note reflect.DeepEqual is not "type safe" - the code will compile even if you did something a bit silly
 	t.Run("comparing two slices with reflect.DeepEqual", func(t *testing.T) {
 		want := []int{3, 9, 62, -23}
@@ -94,4 +93,74 @@ func TestSumAll(t *testing.T) {
 
 	// Next time start with Refactor https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/arrays-and-slices#refactor-2
 
+}
+
+// SumAllAppend
+func TestSumAllAppend(t *testing.T) {
+
+	got := SumAllAppend([]int{1, 2}, []int{0, 9}, []int{20, 5, 4, 33}, []int{-5, -20, 0, 2})
+
+	// Slice can only be cmpared to nil
+	t.Run("testing if slice is nil", func(t *testing.T) {
+
+		if got == nil {
+			t.Errorf("got = %d is nil", got)
+		}
+	})
+
+	// reflect.DeepEqual is useful for seeing if any two variables are the same.
+	// Note reflect.DeepEqual is not "type safe" - the code will compile even if you did something a bit silly
+	t.Run("comparing two slices with reflect.DeepEqual", func(t *testing.T) {
+		want := []int{3, 9, 62, -23}
+		//want := "[3 9 62 -23]"	/    sum_test.go:118: got [3 9 62 -23] want [3 9 62 -23]
+		//--- FAIL: TestSumAllAppend (0.00s) /
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+}
+
+// SumAllTails
+func TestSumAllTails(t *testing.T) {
+
+	//assigning a function to a variable
+	//By defining this function inside the test, it cannot be used by other functions in this package.
+	//Hiding variables and functions that don't need to be exported is an important design consideration.
+	//A handy side-effect of this is this adds a little type-safety to our code.
+	//If a developer mistakenly adds a new test with checkSums(t, got, "dave") the compiler will stop them in their tracks.
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("make the sums of tails of", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		checkSums(t, got, want)
+	})
+
+	/* This test will fail to compile with the following error:
+		--
+		arrays_slices\sum_test.go:154:3: want declared and not used
+		arrays_slices\sum_test.go:155:21: cannot use "dave" (untyped string constant) as []int value in argument to checkSums
+		FAIL    go-examples/arrays_slices [build failed]
+		--
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		checkSums(t, got, "dave")
+
+	})
+	*/
+
+	// recap from https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/arrays-and-slices#wrapping-up
 }
