02ad99471a37d90959a010567e823b1f9e5b5393
Ania <anna.stempel@gmail.com>
2023-08-16 22:12:34 +0100

    Appending and slicing a slice.

--- arrays_slices/sum.go
+++ arrays_slices/sum.go
@@ -48,3 +48,38 @@ func SumAll(numbersToSum ...[]int) []int {
 
 	return sums
 }
+
+/*
+SumAllAppend takes a number of slices and appends a sum of elements in each slice to an empty slice
+https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/arrays-and-slices#refactor-2
+*/
+func SumAllAppend(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, SumSlices(numbers))
+	}
+
+	return sums
+}
+
+
+/*
+SumAllTails takes a number of slice and removes the head item from each slice and then appends the sum of all elements in each slice to an empty slice, 
+returning a slice which is a sum of the tail items of the original slices. If a slice is empty it will append a slice with one item equal 0.
+*/
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			//Slices can be sliced! The syntax is slice[low:high]. 
+			//If you omit the value on one of the sides of the : it captures everything to that side of it. 
+			//In our case, we are saying "take from 1 to the end" with numbers[1:]. 
+			tail := numbers[1:]	
+			sums = append(sums, SumSlices(tail))
+		}
+	}
+
+	return sums
+}
