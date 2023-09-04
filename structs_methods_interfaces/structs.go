c5d815470f33e9c6d026d6ff5f43c3471d93d63c
Ania <anna.stempel@gmail.com>
2023-09-04 21:42:00 +0100

    Introducing Structs, Methods and Interfaces

--- structs_methods_interfaces/structs.go
+++ structs_methods_interfaces/structs.go
@@ -1,4 +1,7 @@
 package structs
+
+import "math"
+
 /*
 	Perimeter takes a width and a height of a rectangle and returns its perimeter (perimeter = 2 * (width + height))
 */
@@ -11,4 +14,65 @@ func Perimeter (width float64, height float64) float64 {
 */
 func Area (width, height float64) float64 {
 	return width * height
+}
+
+
+// Structs
+/*
+	Rectangle - decalring our own type, rectangle struct 
+	You can access the fields of a struct with the syntax of myStruct.field.
+*/
+type Rectangle struct {
+	Width  float64
+	Height float64
+}	
+
+/*
+	Circle struct
+*/
+type Circle struct {
+	Radius float64
+}
+
+/* 
+	StructPerimeter takes Rectangle struct that has 2 fields Width and Height. It returns the rectangle's perimeter.
+*/	
+
+func StructPerimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+/* 
+	StructArea takes Rectangle struct that has 2 fields Width and Height. It returns the rectangle's area.
+*/
+func StructArea(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
+}
+
+// Methods
+/*
+	Area method for Rectangle
+	The syntax for declaring methods is almost the same as functions and that's because they're so similar. 
+	The only difference is the syntax of the method receiver func (receiverName ReceiverType) MethodName(args).
+	When your method is called on a variable of that type, you get your reference to its data via the receiverName variable. 
+	In many other programming languages this is done implicitly and you access the receiver via this.
+	It is a convention in Go to have the receiver variable be the first letter of the type.
+	For example: r Rectangle
+*/
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+/*
+	Area method for Circle
+*/
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+// Interfaces
+/*
+	Interface - decalring our own type as a Shape interface 
+*/
+type Shape interface {
+	Area() float64
 }
--- structs_methods_interfaces/structs_test.go
+++ structs_methods_interfaces/structs_test.go
@@ -21,4 +21,75 @@ func TestArea(t *testing.T) {
 	}
 }
 
-// Next time start with Refactor struct https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/structs-methods-and-interfaces#refactor
+// Struct https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/structs-methods-and-interfaces#refactor
+
+func TestStructPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := StructPerimeter(rectangle)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestStructArea(t *testing.T) {
+	rectangle := Rectangle{12.0, 6.0}
+	got := StructArea(rectangle)
+	want := 72.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+
+func TestMethodArea(t *testing.T) {
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		got := rectangle.Area()
+		want := 72.0
+
+		if got != want {
+			//Use of g will print a more precise decimal number in the error message (fmt options). 
+			//For example, using a radius of 1.5 in a circle area calculation, f would show 7.068583 whereas g would show 7.0685834705770345.
+			t.Errorf("got %g want %g", got, want)	
+		}
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
+
+}
+
+func TestInterfaceArea(t *testing.T) {
+
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		checkArea(t, rectangle, 72.0)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		checkArea(t, circle, 314.1592653589793)
+	})
+
+}
+
+// Next time strat with Decoupling  https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/structs-methods-and-interfaces#decoupling
