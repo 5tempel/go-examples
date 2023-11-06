02244e1d73a0cbcd1a663cec3f3a03ba1c3e3735
Ania <anna.stempel@gmail.com>
2023-11-06 21:47:03 +0000

    refactoring tests and adding a new method BitcoinWithdraw

--- pointers_errors/pointers_errors_test.go
+++ pointers_errors/pointers_errors_test.go
@@ -22,6 +22,7 @@ func TestWallet(t *testing.T) {
 	}
 }
 
+/*
 func TestBitcoinWallet(t *testing.T) {
 
 	wallet := BitcoinWallet{}
@@ -30,12 +31,80 @@ func TestBitcoinWallet(t *testing.T) {
 
 	got := wallet.BitcoinBalance()
 
-	want := Bitcoin(10)
+	want := Bitcoin(100)
 
 	if got != want {
-		t.Errorf("got %d want %d", got, want)
+		t.Errorf("got %s want %s", got, want)
 	}
 }
-
+*/
 // next time start with "Let's implement Stringer on Bitcoin"
 // https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/pointers-and-errors#refactor
+
+/*
+func TestBitcoinWallet(t *testing.T) {
+
+	t.Run("deposit", func(t *testing.T) {
+		wallet := BitcoinWallet{}
+
+		wallet.BitcoinDeposit(Bitcoin(10))
+
+		got := wallet.BitcoinBalance()
+
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := BitcoinWallet{balance: Bitcoin(20)}
+
+		wallet.BitcoinWithdraw(Bitcoin(10))
+
+		got := wallet.BitcoinBalance()
+
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+}
+
+*/
+
+// refactor
+func TestBitcoinWallet(t *testing.T) {
+
+	assertBalance := func(t testing.TB, wallet BitcoinWallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.BitcoinBalance()
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
+
+	t.Run("deposit", func(t *testing.T) {
+		wallet := BitcoinWallet{}
+		wallet.BitcoinDeposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := BitcoinWallet{balance: Bitcoin(20)}
+		wallet.BitcoinWithdraw(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+}
+
+/*
+ next time start here:
+ https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/pointers-and-errors#write-the-test-first-2
+
+ start with:
+ "What should happen if you try to Withdraw more
+ than is left in the account?"
+*/
